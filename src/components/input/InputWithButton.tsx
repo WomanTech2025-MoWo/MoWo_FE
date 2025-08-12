e039e88c38cc250688ad66a615dfc6ad410ee2b5
Sohee Moon <soheetech@gmail.com>
2025-08-12 20:24:00 +0900

    ✨[Feat] InputWithButton 컴포넌트 추가 및 generateId 분리

---
+++ src/components/input/InputWithButton.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+import InputField, { Wrap, Label, StyledInput } from './InputField';
+import { PrimaryButton } from '../button/PrimaryButton';
+import { generateId } from '../../utils/generateId';
+
+type InputWithButtonProps = React.ComponentProps<typeof InputField> & {
+  buttonText?: string;
+  onButtonClick?: () => void;
+};
+
+const InputRow = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+const CheckBtn = styled(PrimaryButton)`
+  width: 86px;
+  height: 45px;
+`;
+
+const StyledInputFlex = styled(StyledInput)`
+  width: auto;
+  flex-grow: 1;
+`;
+
+const InputWithButton = ({ label, id, buttonText = '중복확인', onButtonClick, ...props }: InputWithButtonProps) => {
+  const inputId = id ?? generateId();
+
+  return (
+    <Wrap>
+      {label && <Label htmlFor={inputId}>{label}</Label>}
+      <InputRow>
+        <StyledInputFlex id={inputId} {...props} />
+        <CheckBtn type="button" onClick={onButtonClick}>
+          {buttonText}
+        </CheckBtn>
+      </InputRow>
+    </Wrap>
+  );
+};
+
+export default InputWithButton;
