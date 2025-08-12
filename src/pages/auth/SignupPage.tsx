4e185540a3c35ea31f3add21db40bd9d1e4a8a93
Sohee Moon <soheetech@gmail.com>
2025-08-12 21:07:02 +0900

    ♻️[Refactor] 회원가입 입력 항목 AccountForm 컴포넌트로 분리

--- src/pages/auth/SignupPage.tsx
+++ src/pages/auth/SignupPage.tsx
@@ -1,17 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import InnerLayout from '../../layouts/InnerLayout';
 import HeaderWithBack from '../../layouts/HeaderWithBack';
+import AccountForm from './components/AccountForm';
 import { PrimaryButton } from '../../components/button/PrimaryButton';
-import InputField from '../../components/input/InputField';
-import InputWithButton from '../../components/input/InputWithButton';
 
 const SignupPage = () => {
-  const [memberId, setMemberId] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordCheck, setPasswordCheck] = useState('');
-  const [nickname, setNickname] = useState('');
-  const [birthdate, setBirthdate] = useState('');
-
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault(); // 페이지 새로고침 방지
     // 회원가입 로직
@@ -22,33 +15,7 @@ const SignupPage = () => {
       <HeaderWithBack>회원가입</HeaderWithBack>
       <InnerLayout>
         <form onSubmit={handleSubmit}>
-          <InputField
-            label="이메일 (아이디)"
-            type="email"
-            placeholder="이메일 주소"
-            value={memberId}
-            onChange={(e) => setMemberId(e.target.value)}
-            required
-          />
-          <InputField
-            label="비밀번호"
-            type="password"
-            placeholder="비밀번호"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <InputField
-            label="비밀번호 확인"
-            type="password"
-            placeholder="비밀번호 확인"
-            value={passwordCheck}
-            onChange={(e) => setPasswordCheck(e.target.value)}
-            required
-          />
-          <InputWithButton label="닉네임" type="text" placeholder="닉네임" value={nickname} onChange={(e) => setNickname(e.target.value)} required />
-          <InputField label="생년월일" type="date" placeholder="비밀번호" value={birthdate} onChange={(e) => setBirthdate(e.target.value)} required />
-
+          <AccountForm />
           <PrimaryButton type="submit">회원가입</PrimaryButton>
         </form>
       </InnerLayout>
---
+++ src/pages/auth/components/AccountForm.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import InputField from '../../../components/input/InputField';
+import InputWithButton from '../../../components/input/InputWithButton';
+
+const AccountForm = () => {
+  const [memberId, setMemberId] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [birthdate, setBirthdate] = useState('');
+
+  return (
+    <>
+      <InputField
+        label="이메일 (아이디)"
+        type="email"
+        placeholder="이메일 주소"
+        value={memberId}
+        onChange={(e) => setMemberId(e.target.value)}
+        required
+      />
+      <InputField label="비밀번호" type="password" placeholder="비밀번호" value={password} onChange={(e) => setPassword(e.target.value)} required />
+      <InputField
+        label="비밀번호 확인"
+        type="password"
+        placeholder="비밀번호 확인"
+        value={passwordCheck}
+        onChange={(e) => setPasswordCheck(e.target.value)}
+        required
+      />
+      <InputWithButton label="닉네임" type="text" placeholder="닉네임" value={nickname} onChange={(e) => setNickname(e.target.value)} required />
+      <InputField label="생년월일" type="date" placeholder="비밀번호" value={birthdate} onChange={(e) => setBirthdate(e.target.value)} required />
+    </>
+  );
+};
+
+export default AccountForm;
