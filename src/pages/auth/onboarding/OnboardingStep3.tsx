ae0292457dffebed9c64ec56bc234ee1e35f53bf
Sohee Moon <soheetech@gmail.com>
2025-08-13 15:19:03 +0900

    ✨[Feat] 온보딩 3단계 페이지 UI 구현 및 선택 옵션 1줄 표시 개수 조정

--- src/pages/auth/onboarding/OnboardingStep3.tsx
+++ src/pages/auth/onboarding/OnboardingStep3.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+import { OnboardingWrap, onboardingBaseForm } from './OnboardingStep1';
+import HeaderWithBack from '../../../layouts/HeaderWithBack';
+import InnerLayout from '../../../layouts/InnerLayout';
+import OnboardingIntro from '../components/OnboardingIntro';
+import OnboardingStep3Form from '../components/OnboardingStep3Form';
+import { PrimaryButton } from '../../../components/buttons/PrimaryButton';
+
+const StyledOnboardingStep3Form = styled(OnboardingStep3Form)`
+  ${onboardingBaseForm}
+`;
 
 const OnboardingStep3 = () => {
-  return <></>;
+  return (
+    <OnboardingWrap>
+      <HeaderWithBack />
+      <InnerLayout>
+        <OnboardingIntro />
+        <StyledOnboardingStep3Form />
+        <PrimaryButton to="/">시작하기</PrimaryButton>
+      </InnerLayout>
+    </OnboardingWrap>
+  );
 };
 
 export default OnboardingStep3;
