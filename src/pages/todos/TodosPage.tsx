6011ac3ca9bb7cb3c38a876f966a89880589f267
Sohee Moon <soheetech@gmail.com>
2025-08-15 00:54:35 +0900

    ✨[Feat] TodosPage 레이아웃 구성 및 UI 구현

--- src/pages/todos/TodosPage.tsx
+++ src/pages/todos/TodosPage.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { DayPicker } from 'react-day-picker';
+import { ko } from 'react-day-picker/locale';
+import 'react-day-picker/dist/style.css';
+import GlobalNavigation from '../../layouts/GlobalNavigation';
+import InnerLayout from '../../layouts/InnerLayout';
+import TodoHeader from './components/TodoHeader';
+import TodoList from './components/TodoList';
+
+const TodoWrap = styled(InnerLayout)`
+  background-color: var(--color-basic-bg);
+  min-height: 100vh;
+`;
 
 const TodosPage = () => {
-  return <></>;
+  const [selected, setSelected] = useState<Date>();
+
+  return (
+    <TodoWrap>
+      <TodoHeader />
+      <div>
+        <DayPicker locale={ko} navLayout="around" animate mode="single" selected={selected} onSelect={setSelected} showOutsideDays />
+      </div>
+      <div>
+        <TodoList />
+      </div>
+      <GlobalNavigation />
+    </TodoWrap>
+  );
 };
 
 export default TodosPage;
