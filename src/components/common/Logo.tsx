9dee99ccfd1c4b766a2edae985cc451d0f0eda3f
Sohee Moon <soheetech@gmail.com>
2025-08-16 04:46:39 +0900

    ✨[Feat] Logo 컴포넌트 추가

---
+++ src/components/common/Logo.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+import IconLogo from '../icons/common/IconLogo';
+
+const LogoWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: var(--size-inner-padding-4x) 0;
+`;
+
+const Logo = () => {
+  return (
+    <LogoWrapper>
+      <IconLogo />
+    </LogoWrapper>
+  );
+};
+
+export default Logo;
