489f4d3acc91fbb167811f0f0d96336a6edb0cd9
Sohee Moon <soheetech@gmail.com>
2025-08-16 04:45:48 +0900

    ✨[Feat] 배경색 지정 Context 추가

--- src/layouts/CommonLayout.tsx
+++ src/layouts/CommonLayout.tsx
@@ -6,18 +6,18 @@ interface BgColorContextProps {
   setBgColor: (colorKey: string) => void;
 }
 
-const BgColorContext = createContext<BgColorContextProps | null>(null);
+export const BgColorContext = createContext<BgColorContextProps | null>(null);
 
-const Wrap = styled.div<{ bgColor?: string }>`
+const Wrap = styled.div<{ $bgColor?: string }>`
   width: 100%;
   min-width: var(--view-min-width);
   max-width: var(--view-max-width);
   min-height: var(--view-min-height);
-  background-color: ${({ bgColor }) => (bgColor ? `var(--color-background-${bgColor})` : `var(--color-background-white)`)};
+  background-color: ${({ $bgColor }) => ($bgColor ? `var(--color-background-${$bgColor})` : `var(--color-background-white)`)};
   margin: 0 auto;
   padding-bottom: var(--view-global-nav-height);
-  border-left: 1px solid var(--color-border-color-light);
-  border-right: 1px solid var(--color-border-color-light);
+  border-left: 1px solid var(--color-border-color);
+  border-right: 1px solid var(--color-border-color);
 `;
 
 const CommonLayout = () => {
@@ -29,7 +29,7 @@ const CommonLayout = () => {
 
   return (
     <BgColorContext.Provider value={{ setBgColor: handleColorChange }}>
-      <Wrap bgColor={bgColor}>
+      <Wrap $bgColor={bgColor}>
         <Outlet />
       </Wrap>
     </BgColorContext.Provider>
--- src/layouts/InnerLayout.tsx
+++ src/layouts/InnerLayout.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import styled from 'styled-components';
 import { WithChildren } from '../types/common';
+import { BgColorContext } from './CommonLayout';
+
+interface InnerLayoutProps extends WithChildren {
+  bgColor?: string;
+}
 
 const PageInner = styled.div`
   width: 100%;
   margin: 0 auto;
-  padding: var(--size-layout-padding);
+  padding: var(--size-inner-padding);
 `;
 
-const InnerLayout = ({ children, className }: WithChildren) => {
+const InnerLayout = ({ bgColor, children, className }: InnerLayoutProps) => {
+  const context = useContext(BgColorContext);
+
+  useEffect(() => {
+    if (bgColor && context?.setBgColor) {
+      context.setBgColor(bgColor);
+    }
+  }, [bgColor, context]);
+
   return <PageInner className={className}>{children}</PageInner>;
 };
 
