4654a85220f6dba2ec1b4cf356f9cc5e7de0a0b0
Sohee Moon <soheetech@gmail.com>
2025-08-16 08:48:31 +0900

    ♻️[Refactor] 온보딩 필드셋 CSS 스타일 변수 적용 및 정리

--- src/pages/auth/components/OnboardingFieldset.tsx
+++ src/pages/auth/components/OnboardingFieldset.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-type BaseFieldsetProps = React.HTMLAttributes<HTMLDivElement> & {
+export interface BaseProps extends React.HTMLAttributes<HTMLDivElement> {
+  where: 'onboarding' | 'profile';
+}
+
+export interface BaseFieldsetProps extends BaseProps {
   columns?: number;
-};
+}
 
-const BaseFieldset = styled.div<{ $columns: number }>`
+const BaseFieldset = styled.div<{ $columns: number; $where: 'onboarding' | 'profile' }>`
+
   fieldset {
     display: grid;
     grid-template-columns: repeat(${(props) => props.$columns}, 1fr);
-    gap: 12px 8px;
-    margin-bottom: 16px;
+    gap: var(--size-gap-sm) var(--size-gap-xs);
+    margin-bottom: var(--size-gap-md);
+  }
 
-    legend {
-      margin-bottom: 8px;
-    }
+  ${(props) =>
+    props.$where === 'onboarding' &&
+    css`
+      /* 온보딩 전용 스타일 */
+      background-color: var(--color-background-white);
+      border-radius: var(--size-border-radius-lg);
+      box-shadow: var(--box-shadow-default);
+      margin-bottom: var(--size-gap-lg);
+      padding: var(--size-gap-lg);
+
+      fieldset {
+        margin-bottom: 0;
+      }
+    `}
   }
 `;
 
-const OnboardingFieldset = ({ children, columns = 1, ...props }: BaseFieldsetProps) => {
+const OnboardingFieldset = ({ children, columns = 1, where, ...props }: BaseFieldsetProps) => {
   return (
-    <BaseFieldset $columns={columns} {...props}>
+    <BaseFieldset $columns={columns} $where={where} {...props}>
       <fieldset>{children}</fieldset>
     </BaseFieldset>
   );
