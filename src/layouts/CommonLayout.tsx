6e0090b15dae079a0f2c53e2193a9584bb2d0198
Sohee Moon <soheetech@gmail.com>
2025-08-16 05:42:30 +0900

    ♻️[Refactor] 헤더 영역 padding-top 스타일 조건 추가

--- src/layouts/CommonLayout.tsx
+++ src/layouts/CommonLayout.tsx
@@ -4,32 +4,40 @@ import { Outlet } from 'react-router-dom';
 
 interface BgColorContextProps {
   setBgColor: (colorKey: string) => void;
+  setHeaderPadding: (enabled: boolean) => void;
 }
 
 export const BgColorContext = createContext<BgColorContextProps | null>(null);
 
-const Wrap = styled.div<{ $bgColor?: string }>`
+const Wrap = styled.div<{ $bgColor?: string; $headerPadding?: boolean }>`
   width: 100%;
   min-width: var(--view-min-width);
   max-width: var(--view-max-width);
   min-height: var(--view-min-height);
   background-color: ${({ $bgColor }) => ($bgColor ? `var(--color-background-${$bgColor})` : `var(--color-background-white)`)};
   margin: 0 auto;
+  padding-top: ${({ $headerPadding }) => ($headerPadding ? 'var(--size-header-with-back)' : '0')};
   padding-bottom: var(--view-global-nav-height);
   border-left: 1px solid var(--color-border-color);
   border-right: 1px solid var(--color-border-color);
+  position: relative;
 `;
 
 const CommonLayout = () => {
   const [bgColor, setBgColor] = useState('white');
+  const [headerPadding, setHeaderPadding] = useState(false);
 
   const handleColorChange = (colorKey: string) => {
     setBgColor(colorKey);
   };
 
+  const handleHeaderPaddingChange = (enabled: boolean) => {
+    setHeaderPadding(enabled);
+  };
+
   return (
-    <BgColorContext.Provider value={{ setBgColor: handleColorChange }}>
-      <Wrap $bgColor={bgColor}>
+    <BgColorContext.Provider value={{ setBgColor: handleColorChange, setHeaderPadding: handleHeaderPaddingChange }}>
+      <Wrap $bgColor={bgColor} $headerPadding={headerPadding}>
         <Outlet />
       </Wrap>
     </BgColorContext.Provider>
