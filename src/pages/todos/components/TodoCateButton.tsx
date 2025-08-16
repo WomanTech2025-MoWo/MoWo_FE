113e434a74a29f0b60a74a8a8840e34b43601f8a
Sohee Moon <soheetech@gmail.com>
2025-08-16 12:05:44 +0900

    ♻️[Refactor] 투두 카테고리 버튼 스타일 SegmentedControlStyle로 분리

--- src/pages/todos/components/TodoCateButton.tsx
+++ src/pages/todos/components/TodoCateButton.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { SegmentedContainer, SegmentedList, SegmentedButton } from '../../../components/buttons/ui/SegmentedControlStyle';
 
 type CategoryList = 'all' | 'health' | 'work' | 'personal';
 
@@ -7,42 +8,6 @@ interface TodoCateButtonProps {
   activeCategory: CategoryList;
   onCategoryClick: (name: CategoryList) => void;
 }
-
-const TodoCateButtonWrap = styled.div`
-  background-color: var(--color-gray-200);
-  border-radius: var(--size-border-radius-md);
-  padding: 4px;
-  margin: 16px auto;
-  position: sticky;
-  top: 0;
-  z-index: 1;
-`;
-
-const TodoCateButtonWrapper = styled.ul`
-  display: flex;
-  flex-direction: row;
-
-  li {
-    flex: 1;
-  }
-`;
-
-const CategoryButton = styled.button<{ $active?: boolean }>`
-  background: ${({ $active }) => ($active ? 'var(--color-basic-white)' : 'transparent')};
-  color: ${({ $active }) => ($active ? 'inherit' : 'var(--color-gray-600)')};
-  text-align: center;
-  border-radius: 6px;
-  font-weight: var(--font-weight-medium);
-  width: 100%;
-  height: 36px;
-  box-shadow: ${({ $active }) => ($active ? 'var(--box-shadow-default)' : 'none')};
-
-  &:hover {
-    background: var(--color-basic-white);
-    color: inherit;
-    box-shadow: var(--box-shadow-default);
-  }
-`;
 
 const TodoCateButton = ({ activeCategory, onCategoryClick }: TodoCateButtonProps) => {
   const cateItems: { name: CategoryList; label: string }[] = [
@@ -52,19 +17,19 @@ const TodoCateButton = ({ activeCategory, onCategoryClick }: TodoCateButtonProps
   ];
 
   return (
-    <TodoCateButtonWrap>
-      <TodoCateButtonWrapper>
+    <SegmentedContainer>
+      <SegmentedList>
         {cateItems.map((item) => {
           return (
             <li key={item.name}>
-              <CategoryButton onClick={() => onCategoryClick(item.name)} $active={activeCategory === item.name}>
+              <SegmentedButton onClick={() => onCategoryClick(item.name)} $active={activeCategory === item.name}>
                 {item.label}
-              </CategoryButton>
+              </SegmentedButton>
             </li>
           );
         })}
-      </TodoCateButtonWrapper>
-    </TodoCateButtonWrap>
+      </SegmentedList>
+    </SegmentedContainer>
   );
 };
 
