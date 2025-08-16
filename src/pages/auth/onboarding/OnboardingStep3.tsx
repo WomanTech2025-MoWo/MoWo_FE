5627a9670a5f749e5834f613db8df8ebaea4b819
Sohee Moon <soheetech@gmail.com>
2025-08-16 09:56:24 +0900

    ♻️[Refactor] 온보딩 3단계 레이아웃 코드 정리

--- src/pages/auth/onboarding/OnboardingStep3.tsx
+++ src/pages/auth/onboarding/OnboardingStep3.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
-import { OnboardingWrap, onboardingBaseForm } from './OnboardingStep1';
 import HeaderWithBack from '../../../layouts/HeaderWithBack';
 import InnerLayout from '../../../layouts/InnerLayout';
 import OnboardingIntro from '../components/OnboardingIntro';
 import OnboardingStep3Form from '../components/OnboardingStep3Form';
 import { PrimaryButton } from '../../../components/buttons/PrimaryButton';
 
-const StyledOnboardingStep3Form = styled(OnboardingStep3Form)`
-  ${onboardingBaseForm}
-`;
-
 const OnboardingStep3 = () => {
   return (
-    <OnboardingWrap>
+    <InnerLayout bgColor="gray-light" withHeader={true}>
       <HeaderWithBack />
-      <InnerLayout>
-        <OnboardingIntro />
-        <StyledOnboardingStep3Form />
-        <PrimaryButton to="/">시작하기</PrimaryButton>
-      </InnerLayout>
-    </OnboardingWrap>
+      <OnboardingIntro step="step3" />
+      <OnboardingStep3Form where="onboarding" />
+      <PrimaryButton to="/">시작하기</PrimaryButton>
+    </InnerLayout>
   );
 };
 
