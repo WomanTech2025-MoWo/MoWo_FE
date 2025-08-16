93763fa18b82061f16aca26f3596cd1fc5248e2e
Sohee Moon <soheetech@gmail.com>
2025-08-16 23:47:59 +0900

    ✨[Feat] 주차별 가이드 상세 페이지 이미지 및 스타일 추가

--- src/pages/guides/GuideDetailPage.tsx
+++ src/pages/guides/GuideDetailPage.tsx
@@ -1,12 +1,59 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useParams, Navigate } from 'react-router-dom';
-import { GuideWrap } from './GuidesPage';
 import HeaderWithBack from '../../layouts/HeaderWithBack';
 import InnerLayout from '../../layouts/InnerLayout';
 import GlobalNavigation from '../../layouts/GlobalNavigation';
 import AddTodoButton from './components/AddTodoButton';
 import { weekGuideData } from '../../data/weekGuideData';
+import imgGuidesDetail from '../../assets/features/infos/img-guide-detail.webp';
+import CircleBadge from '../../components/common/CircleBadge';
+import IconBulb from '../../components/icons/features/infos/IconBulb';
+
+const ImageWrapper = styled.div``;
+
+const ContentsWrapper = styled(InnerLayout)`
+  display: flex;
+  flex-direction: column;
+  gap: var(--size-gap-xl);
+  padding-bottom: calc(var(--size-add-todo) + var(--size-inner-padding));
+`;
+
+const QaWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: var(--size-gap-md);
+`;
+
+const Question = styled.h3`
+  font-size: var(--font-size-lg);
+  font-weight: var(--font-weight-semi-bold);
+  line-height: var(--line-height-md);
+  color: var(--color-gray-700);
+`;
+
+const Answer = styled.p`
+  line-height: var(--line-height-md);
+`;
+
+const NoteWrapper = styled.div`
+  background-color: var(--color-background-gray-default);
+  border-radius: var(--size-border-radius-md);
+  padding: var(--size-gap-xl);
+`;
+
+const NoteTitle = styled.h3`
+  display: flex;
+  align-items: center;
+  gap: var(--size-gap-xs);
+  font-size: var(--font-size-lg);
+  font-weight: var(--font-weight-semi-bold);
+  margin-bottom: var(--size-gap-md);
+`;
+
+const NoteText = styled.p`
+  line-height: var(--line-height-md);
+`;
 
 const GuideDetailPage = () => {
   const { period, testname } = useParams<{ period: string; testname: string }>();
@@ -20,27 +67,33 @@ const GuideDetailPage = () => {
   }
 
   return (
-    <GuideWrap>
+    <InnerLayout innerPadding={false} withHeader={true}>
       <HeaderWithBack>{testData.testName}</HeaderWithBack>
-      <div>
-        <InnerLayout>
-          {testData.details.questions.map((qa, index) => (
-            <div key={index}>
-              <h3>Q. {qa.question}</h3>
-              <p>{qa.answer}</p>
-            </div>
-          ))}
-          {testData.details.note && (
-            <div>
-              <h3>기억하면 좋아요</h3>
-              <p>{testData.details.note}</p>
-            </div>
-          )}
-        </InnerLayout>
-      </div>
+      <ImageWrapper>
+        <img src={`${imgGuidesDetail}`} alt={testData.testName} />
+      </ImageWrapper>
+      <ContentsWrapper>
+        {testData.details.questions.map((qa, index) => (
+          <QaWrapper key={index}>
+            <Question>
+              <CircleBadge value="Q" circleColor="var(--color-secondary-primary)" label={qa.question} />
+            </Question>
+            <Answer>{qa.answer}</Answer>
+          </QaWrapper>
+        ))}
+        {testData.details.note && (
+          <NoteWrapper>
+            <NoteTitle>
+              <IconBulb />
+              기억하면 좋아요
+            </NoteTitle>
+            <NoteText>{testData.details.note}</NoteText>
+          </NoteWrapper>
+        )}
+      </ContentsWrapper>
       <AddTodoButton />
       <GlobalNavigation />
-    </GuideWrap>
+    </InnerLayout>
   );
 };
 
