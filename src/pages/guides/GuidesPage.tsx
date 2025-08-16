100fac57341fb6d46d3d41875e33a3aa1f70cb98
Sohee Moon <soheetech@gmail.com>
2025-08-16 13:12:09 +0900

    ✨[Feat] 완주 배너 추가 및 CSS 스타일 변수 적용

--- src/pages/guides/GuidesPage.tsx
+++ src/pages/guides/GuidesPage.tsx
@@ -1,30 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import HeaderWithBack from '../../layouts/HeaderWithBack';
 import InnerLayout from '../../layouts/InnerLayout';
 import IconArrowRight from '../../components/icons/common/IconArrowRight';
 import { weekGuideData } from '../../data/weekGuideData';
+import bgGuidesDone from '../../assets/features/infos/bg-guides-done.png';
 
-export const GuideWrap = styled.div`
-  background-color: var(--color-basic-bg);
-  min-height: 100vh;
-  position: relative;
-`;
+export const GuideWrap = styled(InnerLayout)``;
 
-const GuideWrapper = styled.div`
+const GuideWrapper = styled.ul`
   display: flex;
   flex-direction: column;
-  gap: 18px;
+  gap: var(--size-gap-md);
 `;
 
-const PeriodBox = styled.div`
-  display: flex;
-  gap: 0 14px;
-  background-color: var(--color-basic-white);
+const baseBoxStyle = css`
   border-radius: var(--size-border-radius-md);
   box-shadow: var(--box-shadow-default);
-  padding: var(--size-layout-padding);
+  padding: var(--size-gap-lg);
+  display: flex;
+`;
+
+const PeriodBox = styled.li`
+  ${baseBoxStyle}
+
+  gap: 0 var(--size-gap-md);
+  background-color: var(--color-background-white);
   word-break: keep-all;
   overflow-wrap: break-word;
   white-space: normal;
@@ -39,13 +41,13 @@ const PeriodBox = styled.div`
   }
 
   &:first-child > ul::before {
-    top: 8px;
-    height: calc(100% + 20px - 8px);
+    top: var(--size-gap-xs);
+    height: calc(100% + var(--size-gap-lg) - var(--size-gap-xs));
   }
 `;
 
 export const TitleWrapper = styled.div`
-  width: 80px;
+  width: var(--size-inner-padding-4x);
   font-size: var(--font-size-sm);
   font-weight: var(--font-weight-medium);
   line-height: var(--line-height-xs);
@@ -55,7 +57,7 @@ export const TitleWrapper = styled.div`
 export const TestList = styled.ul`
   display: flex;
   flex-direction: column;
-  gap: 36px;
+  gap: var(--size-gap-xxl);
   position: relative;
 
   &::before {
@@ -72,7 +74,7 @@ export const TestList = styled.ul`
 
 export const TestItem = styled.li`
   position: relative;
-  padding-left: 24px;
+  padding-left: var(--size-gap-xl);
 
   &::before {
     content: '';
@@ -91,37 +93,57 @@ export const TestItem = styled.li`
     flex-direction: row;
     align-items: center;
     font-weight: var(--font-weight-medium);
-    gap: 10px;
+    gap: var(--size-gap-xs);
   }
+`;
+
+const DoneBox = styled.li`
+  ${baseBoxStyle}
+
+  background-color: var(--color-secondary-300);
+  background-image: url(${bgGuidesDone});
+  background-position: center center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: var(--size-height-banner);
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  font-size: var(--font-size-xl);
+  line-height: var(--line-height-lg);
+  font-weight: var(--font-weight-semi-bold);
 `;
 
 const GuidesPage = () => {
   return (
-    <GuideWrap>
+    <InnerLayout bgColor="gray-light" withHeader={true}>
       <HeaderWithBack>주차별 가이드</HeaderWithBack>
-      <InnerLayout>
-        <GuideWrapper>
-          {weekGuideData.map((week, index) => (
-            <PeriodBox key={index}>
-              <TitleWrapper>
-                <h3>{week.period}</h3>
-                <p>{week.category}</p>
-              </TitleWrapper>
-              <TestList>
-                {week.tests.map((test, i) => (
-                  <TestItem key={i}>
-                    <Link to={`/guides/${encodeURIComponent(week.period)}/${encodeURIComponent(test.testName)}`}>
-                      {test.testName}
-                      <IconArrowRight />
-                    </Link>
-                  </TestItem>
-                ))}
-              </TestList>
-            </PeriodBox>
-          ))}
-        </GuideWrapper>
-      </InnerLayout>
-    </GuideWrap>
+      <GuideWrapper>
+        {weekGuideData.map((week, index) => (
+          <PeriodBox key={index}>
+            <TitleWrapper>
+              <h3>{week.period}</h3>
+              <p>{week.category}</p>
+            </TitleWrapper>
+            <TestList>
+              {week.tests.map((test, i) => (
+                <TestItem key={i}>
+                  <Link to={`/guides/${encodeURIComponent(week.period)}/${encodeURIComponent(test.testName)}`}>
+                    {test.testName}
+                    <IconArrowRight />
+                  </Link>
+                </TestItem>
+              ))}
+            </TestList>
+          </PeriodBox>
+        ))}
+        <DoneBox>
+          40주의 긴 여정,
+          <br />
+          완주를 축하드려요!
+        </DoneBox>
+      </GuideWrapper>
+    </InnerLayout>
   );
 };
 
