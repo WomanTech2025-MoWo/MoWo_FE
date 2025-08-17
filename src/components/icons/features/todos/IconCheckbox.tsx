9dfb98afbba43da6910c1b0c659ee38d3d8bf141
Sohee Moon <soheetech@gmail.com>
2025-08-17 17:08:01 +0900

    ✨[Feat] IconCheckbox false 값일 때 SVG 추가

--- src/components/icons/features/todos/IconCheckbox.tsx
+++ src/components/icons/features/todos/IconCheckbox.tsx
@@ -3,19 +3,27 @@ import { IconSvgProps } from '../../../../types/common';
 import { ReactComponent as IconCheckHealth } from '../../../../assets/features/todos/icon-check-health.svg';
 import { ReactComponent as IconCheckWork } from '../../../../assets/features/todos/icon-check-work.svg';
 import { ReactComponent as IconCheckPersonal } from '../../../../assets/features/todos/icon-check-personal.svg';
+import { ReactComponent as IconCheckFalse } from '../../../../assets/features/todos/icon-check-false.svg';
 
 interface IconCheckboxProps extends IconSvgProps {
-  status?: 'health' | 'work' | 'personal';
+  category?: 'health' | 'work' | 'personal';
+  status?: boolean;
 }
 
-const IconCheckbox = ({ status = 'health', width = '20px', height = '20px', viewBox = '0 0 20 20' }: IconCheckboxProps) => {
-  if (status === 'health') {
+const IconCheckbox = ({ category = 'health', status = false, width = '20', height = '20', viewBox = '0 0 20 20' }: IconCheckboxProps) => {
+  if (status === false) {
+    return (
+      <>
+        <IconCheckFalse width={width} height={height} viewBox={viewBox} />
+      </>
+    );
+  } else if (category === 'health') {
     return (
       <>
         <IconCheckHealth width={width} height={height} viewBox={viewBox} />
       </>
     );
-  } else if (status === 'work') {
+  } else if (category === 'work') {
     return (
       <>
         <IconCheckWork width={width} height={height} viewBox={viewBox} />
