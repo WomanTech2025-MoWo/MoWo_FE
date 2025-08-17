85423be1d8df3d5bc776260008117d528f3ded99
Sohee Moon <soheetech@gmail.com>
2025-08-17 17:07:27 +0900

    ♻️[Refactor] IconBookmark 색상 변경 가능하게 수정

--- src/components/icons/features/infos/IconBookmark.tsx
+++ src/components/icons/features/infos/IconBookmark.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 import { IconSvgProps } from '../../../../types/common';
 import { ReactComponent as IconBookmarkEmpty } from '../../../../assets/features/infos/icon-bookmark-empty.svg';
 import { ReactComponent as IconBookmarkFill } from '../../../../assets/features/infos/icon-bookmark-fill.svg';
 
 interface IconBookmarkProps extends IconSvgProps {
   status?: 'fill' | 'empty';
+  fillColor?: string;
 }
 
-const IconBookmark = ({ status = 'empty', width = '20px', height = '20px', viewBox = '0 0 20 20' }: IconBookmarkProps) => {
+const StyledFill = styled(IconBookmarkFill)<{ $color?: string }>`
+  path {
+    fill: ${({ $color }) => ($color ? `var(--color-${$color})` : 'var(--color-gray-500)')};
+    stroke: ${({ $color }) => ($color ? `var(--color-${$color})` : 'var(--color-gray-500)')};
+  }
+`;
+
+const IconBookmark = ({ status = 'empty', fillColor, width = '20px', height = '20px', viewBox = '0 0 20 20' }: IconBookmarkProps) => {
   if (status === 'fill') {
-    return (
-      <>
-        <IconBookmarkFill width={width} height={height} viewBox={viewBox} />
-      </>
-    );
+    return <StyledFill width={width} height={height} viewBox={viewBox} $color={fillColor} />;
   }
   return (
     <>
