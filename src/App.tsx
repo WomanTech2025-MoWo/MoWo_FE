e1eabb915e92715a24c00921b1caa8956772de07
Sohee Moon <soheetech@gmail.com>
2025-08-17 20:14:01 +0900

    ✨[Feat] Vercel Web Analytics 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AppRouter from './routes/AppRouter';
+import { Analytics } from '@vercel/analytics/react';
 
 const App = () => {
   return (
     <div className="App">
       <AppRouter />
+      <Analytics />
     </div>
   );
 };
