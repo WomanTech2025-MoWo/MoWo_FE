d3ed0b61720529dd41f18e87d93f7368d461fc15
Sohee Moon <soheetech@gmail.com>
2025-08-20 03:08:15 +0900

    ✨[Feat] 투두 검색 페이지 추가

--- src/pages/todos/components/TodoListItem.tsx
+++ src/pages/todos/components/TodoListItem.tsx
@@ -4,7 +4,7 @@ import IconCheckbox from '../../../components/icons/features/todos/IconCheckbox'
 import IconMeatball from '../../../components/icons/features/todos/IconMeatball';
 import TodoEditDeleteButtons from '../../../components/common/TodoEditDeleteButtons';
 
-type TodoListItemProps = {
+export type TodoListItemProps = {
   id: number;
   text: string;
   category?: 'health' | 'work' | 'personal' | undefined;
@@ -15,9 +15,12 @@ type TodoListItemProps = {
   showCheckbox?: boolean; // 기본 true
   disableCheck?: boolean; // 기본 false
   className?: string;
+  todoDate?: string;
 };
 
-const TodoRow = styled.li`
+const TodoRow = styled.li``;
+
+const TodoContentWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -27,6 +30,13 @@ const TodoContent = styled.div`
   display: flex;
   align-items: center;
   gap: var(--size-gap-sm);
+`;
+
+const TodoDate = styled.p`
+  padding-left: calc(20px + var(--size-gap-sm));
+  font-size: var(--font-size-sm);
+  color: var(--color-text-gray-300);
+  margin-top: var(--size-gap-xs);
 `;
 
 const TodoListItem = ({
@@ -40,8 +50,10 @@ const TodoListItem = ({
   showCheckbox = true,
   disableCheck = false,
   className,
+  todoDate,
 }: TodoListItemProps) => {
   const [isChecked, setIsChecked] = useState(checked);
+  const formatTodoDate = todoDate;
 
   const handleCheck = () => {
     if (!disableCheck) {
@@ -49,15 +61,27 @@ const TodoListItem = ({
     }
   };
 
+  const formatDate = (dateStr: string) => {
+    const date = new Date(dateStr);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1; // getMonth는 0~11
+    const day = date.getDate();
+
+    return `${year}년 ${month.toString().padStart(2, '0')}월 ${day.toString().padStart(2, '0')}일`;
+  };
+
   return (
     <TodoRow key={id} className={className}>
-      <TodoContent onClick={handleCheck}>
-        {showCheckbox && <IconCheckbox category={category} status={isChecked} />}
-        <span>{text}</span>
-      </TodoContent>
-      <button type="button" onClick={onOpen}>
-        <IconMeatball width="14" />
-      </button>
+      <TodoContentWrapper>
+        <TodoContent onClick={handleCheck}>
+          {showCheckbox && <IconCheckbox category={category} status={isChecked} />}
+          <span>{text}</span>
+        </TodoContent>
+        <button type="button" onClick={onOpen}>
+          <IconMeatball width="14" />
+        </button>
+      </TodoContentWrapper>
+      {todoDate && <TodoDate>{formatDate(todoDate)}</TodoDate>} {/* 검색페이지용 표시 */}
       {isOpen && <TodoEditDeleteButtons id={id} onClose={onClose} />}
     </TodoRow>
   );
