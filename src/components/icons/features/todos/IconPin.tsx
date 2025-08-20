5902db838da00f2104387c6beec104f9c33242f2
Sohee Moon <soheetech@gmail.com>
2025-08-20 18:35:48 +0900

    ♻️[Refactor] IconPin className 추가

--- src/components/icons/features/todos/IconPin.tsx
+++ src/components/icons/features/todos/IconPin.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { IconSvgProps } from '../../../../types/common';
 import { ReactComponent as SvgIconPin } from '../../../../assets/features/todos/icon-pin.svg';
 
-const IconPin = ({ width = '20px', height = '20px', viewBox = '0 0 20 20' }: IconSvgProps) => {
+const IconPin = ({ width = '20px', height = '20px', viewBox = '0 0 20 20', className }: IconSvgProps) => {
   return (
     <>
-      <SvgIconPin width={width} height={height} viewBox={viewBox} />
+      <SvgIconPin className={className} width={width} height={height} viewBox={viewBox} />
     </>
   );
 };
