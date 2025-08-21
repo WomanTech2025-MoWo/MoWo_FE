436ed5970462a9dd3a94da8a76c99facf4c68fc1
Sohee Moon <soheetech@gmail.com>
2025-08-21 21:09:17 +0900

    ♻️[Refactor] IconAlarm calssName 추가

--- src/components/icons/common/IconAlarm.tsx
+++ src/components/icons/common/IconAlarm.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { IconSvgProps } from '../../../types/common';
 import { ReactComponent as SvgIconAlarm } from '../../../assets/common/icon-alarm.svg';
 
-const IconAlarm = ({ width = '20px', height = '20px', viewBox = '0 0 20 20' }: IconSvgProps) => {
+const IconAlarm = ({ className, width = '20px', height = '20px', viewBox = '0 0 20 20' }: IconSvgProps) => {
   return (
     <>
-      <SvgIconAlarm width={width} height={height} viewBox={viewBox} />
+      <SvgIconAlarm className={className} width={width} height={height} viewBox={viewBox} />
     </>
   );
 };
